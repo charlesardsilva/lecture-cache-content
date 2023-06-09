e6b914663c56ae173ba87895f1c2bdc52910d2f4
Charles Silva <charles.silva@hotmart.com>
2023-06-08 22:28:38 -0300

    Add application code

---
+++ src/main/java/dev/charlesardsilva/lecturecachecontent/LectureCacheContentApplication.java
@@ -0,0 +1,13 @@
+package dev.charlesardsilva.lecturecachecontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureCacheContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureCacheContentApplication.class, args);
+	}
+
+}
